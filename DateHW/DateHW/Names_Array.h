29997c65a9735ff668e5f1a7aa323009743de2ff
n1ke71 <n1ke71@MacBook-Pro-Ivan.local>
2018-02-19 21:04:05 +0300

    Names Array

---
+++ DateHW/DateHW/Names_Array.h
@@ -0,0 +1,26 @@
+static NSString* firstNames[] = {
+    @"Tran", @"Lenore", @"Bud", @"Fredda", @"Katrice",
+    @"Clyde", @"Hildegard", @"Vernell", @"Nellie", @"Rupert",
+    @"Billie", @"Tamica", @"Crystle", @"Kandi", @"Caridad",
+    @"Vanetta", @"Taylor", @"Pinkie", @"Ben", @"Rosanna",
+    @"Eufemia", @"Britteny", @"Ramon", @"Jacque", @"Telma",
+    @"Colton", @"Monte", @"Pam", @"Tracy", @"Tresa",
+    @"Willard", @"Mireille", @"Roma", @"Elise", @"Trang",
+    @"Ty", @"Pierre", @"Floyd", @"Savanna", @"Arvilla",
+    @"Whitney", @"Denver", @"Norbert", @"Meghan", @"Tandra",
+    @"Jenise", @"Brent", @"Elenor", @"Sha", @"Jessie"
+};
+
+static NSString* lastNames[] = {
+    
+    @"Farrah", @"Laviolette", @"Heal", @"Sechrest", @"Roots",
+    @"Homan", @"Starns", @"Oldham", @"Yocum", @"Mancia",
+    @"Prill", @"Lush", @"Piedra", @"Castenada", @"Warnock",
+    @"Vanderlinden", @"Simms", @"Gilroy", @"Brann", @"Bodden",
+    @"Lenz", @"Gildersleeve", @"Wimbish", @"Bello", @"Beachy",
+    @"Jurado", @"William", @"Beaupre", @"Dyal", @"Doiron",
+    @"Plourde", @"Bator", @"Krause", @"Odriscoll", @"Corby",
+    @"Waltman", @"Michaud", @"Kobayashi", @"Sherrick", @"Woolfolk",
+    @"Holladay", @"Hornback", @"Moler", @"Bowles", @"Libbey",
+    @"Spano", @"Folson", @"Arguelles", @"Burke", @"Rook"
+};
